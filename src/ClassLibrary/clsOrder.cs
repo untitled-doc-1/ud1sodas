31d1d996e03998296012a2eec6756fbf7706481e
Arawn Davies <arawn.davies@gmail.com>
2022-02-15 01:52:09 +0000

    feat: initial tests

---
+++ src/ClassLibrary/clsOrder.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class clsOrder
+    {
+#warning implement with class def for stock & inventory (Product Class)
+        public clsOrder(int id, string desc, clsOrder[] prods)
+        {
+            Id = id;
+            Description = desc;
+            Products = prods;
+        }
+
+#warning implement with class def for stock & inventory (Product Class)
+        private clsOrder[] Products;
+
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal TotalCost 
+        {
+            get
+            {
+                decimal itemTotals = 0M;
+                try
+                {
+                    foreach (var item in this.Products)
+                    {
+                        itemTotals += item.TotalCost;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    Console.WriteLine("Setting default arbitrary value");
+                    decimal num = 234.23M;
+                    itemTotals = num;
+                }
+                
+
+                return itemTotals;
+            }
+        }
+    }
+}
---
+++ src/Testing1/tstOrder.cs
@@ -0,0 +1,33 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class
+            clsOrder exampleOrder = new clsOrder(1, "InstanceOK", null);
+            //test for existance
+            Assert.IsNotNull(exampleOrder);
+        }
+
+        [TestMethod]
+        public void DefaultValue()
+        {
+            clsOrder[] arrOrder = new clsOrder[2]
+            {
+                new clsOrder(5, "arrOrder1", null),
+                new clsOrder(10, "arrOrder2", null)
+            };
+            clsOrder aOrder = new clsOrder(0, "ExampleOrderLine", null);
+            clsOrder bOrder = new clsOrder(1, "Example B OrderLine", arrOrder);
+
+            Assert.AreEqual(aOrder.TotalCost * 2, bOrder.TotalCost);
+        }
+    }
+}
