44a9e73a342deea0df8421a493145e6cc180e162
Mohammad Eizeddin <meizeddin@gmail.com>
2022-02-19 12:03:38 +0000

    All initial tests for table porperties are done

--- src/ClassLibrary/clsStaff.cs
+++ src/ClassLibrary/clsStaff.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public bool Active { get; set; }
+        public DateTime DateHired { get; set; }
+        public int EmployeeIDPrimary { get; set; }
+        public double Salary { get; set; }
+        public string EmpFullName { get; set; }
+        public string Permissions { get; set; }
+        public string JobDescription { get; set; }
     }
 }
