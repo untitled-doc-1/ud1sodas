a14c0796a03ef14364d00658aa447a342490d1e5
Mohammad <meizeddin@gmail.com>
2022-02-22 13:33:30 +0000

    Solved the problem with staff viewer

--- src/AdminSystem/StaffViewer.aspx.cs
+++ src/AdminSystem/StaffViewer.aspx.cs
@@ -13,19 +13,19 @@ public partial class _1Viewer : System.Web.UI.Page
         //create a new instance of a clsStaff
         clsStaff AStaff = new clsStaff();
         //get the data from the session object
-        AStaff = (clsStaff)Session["Employee Name "];
+        AStaff = (clsStaff)Session["AStaff"];
         //display employee name for this entry
-        Response.Write(AStaff.EmpFullName);
+        Response.Write(AStaff.EmpFullName + "<br />");
         //display Active status
-        Response.Write(AStaff.Active);
+        Response.Write(AStaff.Active + "<br />");
         //display Job Description and Permissions
-        Response.Write(AStaff.JobDescriptionPermissions);
+        Response.Write(AStaff.JobDescriptionPermissions + "<br />");
         //display employee Id
-        Response.Write(AStaff.EmployeeIDPrimary);
+        Response.Write(AStaff.EmployeeIDPrimary + "<br />");
         //display Staff date of hiring
-        Response.Write(AStaff.DateHired);
+        Response.Write(AStaff.DateHired + "<br />");
         //display staff salary
-        Response.Write(AStaff.Salary);
+        Response.Write(AStaff.Salary + "<br />");
 
     }
 }
