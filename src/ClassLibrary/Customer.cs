902214e286b2728726ba312ddcc1c9806c03dd1b
GOATS2K <jp@glattetre.com>
2022-02-23 15:04:05 +0000

    feat(testing3): add tests for customer class

---
+++ src/ClassLibrary/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing3
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime SignedUpDate { get; set; }
+        public string Address { get; set; }
+        public int PhoneNumber { get; set; }
+        public bool Disabled { get; set; }
+    }
+}
---
+++ src/Testing3/tstCustomer.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing3
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        [DataRow("FullName", "Test Testsson")]
+        [DataRow("Email", "test.testsson@test.se")]
+        [DataRow("PasswordHash", "AAABBBCCC111222333")]
+        [DataRow("Address", "Test me!")]
+        [DataRow("PhoneNumber", 01110001233)]
+        [DataRow("Disabled", false)]
+        public void Customer_InstantiateWithSingleProperties_InstantiatesProperly(string property, object value)
+        {
+            // arrange
+            var customer = new Customer();
+            var customerProperty = customer.GetType().GetProperty(property);
+            // act
+            customerProperty.SetValue(customer, value);
+            // assert
+            Assert.AreEqual(value, customerProperty.GetValue(customer));
+
+        }
+
+        public void Customer_InstatiateWithoutProperties_InstantiatesProperly()
+        {
+            // arrange
+            // act
+            var customer = new Customer();
+            // assert
+            Assert.IsNotNull(customer);
+        }
+
+        [TestMethod]
+        public void Customer_InstatiateWithSingleDateTimeProperty_InstatiatesPropery()
+        {
+            // arrange
+            var customer = new Customer();
+            var timeNow = DateTime.Now;
+            // act
+            customer.SignedUpDate = timeNow;
+            // assert
+            Assert.AreEqual(timeNow, customer.SignedUpDate);
+        }
+    }
+}
