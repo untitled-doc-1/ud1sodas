5ca4927bfc7cc0752d0ae15b4d78eeb88cbeb1d3
P2607465 <P2607465@my365.dmu.ac.uk>
2022-02-23 15:37:26 +0000

    Added testing for the supplier class

---
+++ src/Testing5/tstSupplier.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstSupplier
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create
+            clsSupplier ASupplier = new clsSupplier();
+            //test to see it exists
+            Assert.IsNotNull(ASupplier);
+        }
+    }
+}
