793aff709fa6bc7411706b831906a032957d66fd
GOATS2K <jp@glattetre.com>
2022-03-28 15:20:39 +0100

    feat: finish up week 21, unit 7

--- src/AdminSystem/CustomerViewer.aspx.cs
+++ src/AdminSystem/CustomerViewer.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,8 @@ public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        Customer customer = new Customer();
+        customer = (Customer)Session["ACustomer"];
+        Response.Write(customer.FullName);
     }
 }
